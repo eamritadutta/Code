f6bdd988c270164d788fae19c04708f00d74503c
Amrita Dutta <eamritadutta@gmail.com>
2013-07-19 22:00:20 -0700

    Update ContiguousSeqWithLargestSum.java

--- Arrays/ContiguousSeqWithLargestSum.java
+++ Arrays/ContiguousSeqWithLargestSum.java
@@ -1,33 +1,32 @@
 // This class finds the longest continuous array elements with the largest sum
 
+// This solution finds the largest cont sub seq with max sum even if the array contains all -ve elements. 
+// In this case, the program returns min -ve element.
+
 public class ContiguousSeqWithLargestSum {
     
     public static void main(String[] args) {
-  	int[] arr = {2, 3, -8, -1, 2, 4, -2, 3};
-	
-	printLargestContSubSeq(arr);
+	int[] arr1 = {2, 3, -8, -1, 2, 4, -2, 3};
+	printLargestContSubSeq(arr1);
+
+	int[] arr2 = {-20, -3, -8, -1, -12, -4, -25, -31};
+	printLargestContSubSeq(arr2);
     }
 
-    // this solution assumes that 'arr' will have both +ve and -ve elements
     private static void printLargestContSubSeq(int[] arr) {
 	int maxSum = arr[0];
 	int sum = arr[0];
-	int st = 0; 
-	int end = 0;
-
+	
 	for (int i = 1; i < arr.length; i++) {
 	    sum += arr[i];
 	    
-	    if (sum <= 0) {
-		st = i+1;
-		sum = 0;
-		continue;
-	    }
-
 	    if (sum > maxSum) {
 		maxSum = sum;
-		end = i;
 	    } 
+
+	    if (sum <= 0) {
+		sum = 0;
+	    }
 	}
 
 	System.out.println("The sum of the elements forming the largest contiguous sum is: " + maxSum);
